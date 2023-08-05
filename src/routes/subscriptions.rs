1fa7a35721903a29d6b246d54dfe434183bbadc0
garrettladley <ladley.g@northeastern.edu>
2023-08-05 13:17:57 -0400

    progress

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,29 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::{query, PgPool};
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub email: String,
+    pub name: String,
+}
+
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
