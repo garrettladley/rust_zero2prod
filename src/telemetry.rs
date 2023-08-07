47165475719bfaffe569390581d461582b003489
garrettladley <ladley.g@northeastern.edu>
2023-08-06 22:02:23 -0400

    fixed typos

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,6 +1,7 @@
 use tracing::subscriber::set_global_default;
+use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LigTracer;
+use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
@@ -18,7 +19,7 @@ where
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer);
+        .with(formatting_layer)
 }
 
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
