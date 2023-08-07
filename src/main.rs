077609a8126ced1174fff145a6437d931b14b25b
garrettladley <ladley.g@northeastern.edu>
2023-08-06 22:15:53 -0400

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use env_logger::Env;
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -13,7 +12,7 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
+        PgPool::connect(configuration.database.connection_string().expose_secret())
             .await
             .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
